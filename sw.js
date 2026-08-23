cd228ca94443f5d9dbb39b1d70b27d717bbedb74
sarabek18 <arsazet.17.go@gmail.com>
2026-08-23 11:53:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='combo-keno-shell-v15';
+const CACHE='combo-keno-shell-v16';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./combo-presets-v1.json','./keno-payouts-v1.json'];
 
 self.addEventListener('install',e=>e.waitUntil(
